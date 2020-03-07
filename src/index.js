9e30c12ebda6bdcf8814933b8f4ed3f22e81cb34
Siebe Vander Henst <siebe.vanderhenst@stacked.be>
2020-03-07 16:58:10 +0100

    chore(README): update readme bugFix(abstractField): change error to english text

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import cheetahForm from './formGenerator.vue';
 import formGroup from './formGroup.vue';
-import fields from './utils/fieldsLoader';
 
 const components = {
-  ...fields,
   cheetahForm,
   formGroup,
 };
